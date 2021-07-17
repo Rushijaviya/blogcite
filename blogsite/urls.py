b0115b9bcd684376530b5264d3573a52774190a0
unknown <rushijaviya05@gmail.com>
2021-07-17 19:14:15 +0530

    media image fixed

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls', namespace='blogapp')),
-] 
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
 
 
 if settings.DEBUG:
