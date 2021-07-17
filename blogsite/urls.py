8fde89a062a79f613347414b217171655b605c38
unknown <rushijaviya05@gmail.com>
2021-07-17 19:49:33 +0530

    media url changed

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -19,10 +19,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls', namespace='blogapp')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
+
 
 
 # if settings.DEBUG:
