afe83bc70db06dab60909d3bd00fbc5686334159
unknown <rushijaviya05@gmail.com>
2021-07-17 16:25:15 +0530

    deployment started

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = (
-    'E:/Code/pp/blogsite/templates/blog/post/static',
+    'blogsite/templates/blog/post/static',
 )
 
 
