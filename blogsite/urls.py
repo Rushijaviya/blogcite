876c4c49388b7bf5227d7bdae1a212e2b0daf7da
unknown <rushijaviya05@gmail.com>
2021-07-17 19:17:51 +0530

    media image fixed

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
 
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
